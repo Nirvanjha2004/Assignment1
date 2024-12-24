b87728763d9de84abdee37a9084ee408dc955d80
Nirvanjha2004 <nirvanjha2004@gmail.com>
2024-12-24 17:48:06 +0530

    hi

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@firebase/auth': '@firebase/auth/dist/esm/index.esm.js',
-      '@firebase/firestore': '@firebase/firestore/dist/esm/index.esm.js',
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
     }
   },
   optimizeDeps: {
-    include: ['firebase/app', 'firebase/firestore', 'firebase/auth']
-  },
+    include: ['@firebase/app', '@firebase/firestore', '@firebase/auth']
+  }
 })
