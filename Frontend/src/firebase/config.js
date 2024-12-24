4229117b542a25696d8b509296a525fcc91722b2
Nirvanjha2004 <nirvanjha2004@gmail.com>
2024-12-24 17:43:06 +0530

    hi

--- Frontend/src/firebase/config.js
+++ Frontend/src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from '@firebase/firestore';
+import { getAuth } from '@firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCLpOflc7CTF-MRQ1Wcy7ylcXTRZMDR76U",
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@firebase/auth': '@firebase/auth/dist/esm/index.esm.js',
+      '@firebase/firestore': '@firebase/firestore/dist/esm/index.esm.js',
+    }
+  },
   optimizeDeps: {
-    include: ['firebase/app', 'firebase/firestore'],
+    include: ['firebase/app', 'firebase/firestore', 'firebase/auth']
   },
 })
